06b0f2fa1b05cddff96713158b9b1a1df62fc404
sheebat <sheeba.thukral@gmail.com>
2017-03-03 15:57:48 -0800

    Create script5.js

---
+++ script5.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Sheeba on 2/28/2017.
+ */
+var app = angular.module('myApp', []);
+app.controller('q5Controller', function($scope, $http) {
+    /*$http.get("\ teachersData.json").then(function (response) {
+     $scope.myData = response.data.records;*/
+
+});
