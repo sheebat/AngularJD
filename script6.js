591a7a15622207568e91cad9272246f8726c27e5
sheebat <sheeba.thukral@gmail.com>
2017-03-03 15:56:20 -0800

    Create script6.js

---
+++ script6.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Sheeba on 2/28/2017.
+ */
+var app = angular.module('myApp', []);
+app.controller('q6Controller', function($scope, $http) {
+    /*$http.get("\ teachersData.json").then(function (response) {
+     $scope.myData = response.data.records;*/
+
+});
+
+
+app.controller('q8Controller', function($scope, $http) {
+    /*$http.get("\ teachersData.json").then(function (response) {
+     $scope.myData = response.data.records;*/
+
+});
+
+
+app.controller('q9Controller', function($scope, $http) {
+    /*$http.get("\ teachersData.json").then(function (response) {
+     $scope.myData = response.data.records;*/
+
+});
+
+app.controller('q10Controller', function($scope, $http) {
+    /*$http.get("\ teachersData.json").then(function (response) {
+     $scope.myData = response.data.records;*/
+
+});
+app.controller('thanksController', function($scope, $http) {
+    /*$http.get("\ teachersData.json").then(function (response) {
+     $scope.myData = response.data.records;*/
+
+});
