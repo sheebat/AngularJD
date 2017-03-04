e64f2d305a46739c0ed0edd27ba30202a2b864d9
sheebat <sheeba.thukral@gmail.com>
2017-03-03 16:18:49 -0800

    Create script4.js

---
+++ script4.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Sheeba on 2/27/2017.
+ */
+var app = angular.module('myApp', []);
+app.controller('myController', function($scope, $http) {
+    /*$http.get("\ teachersData.json").then(function (response) {
+        $scope.myData = response.data.records;*/
+
+});
